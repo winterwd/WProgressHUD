ccf5011a1e8c6fd938a69f90dabcd6021a13ba8b
winter <winter.wd@qq.com>
2020-02-12 14:15:09 +0800

    suport carthage

---
+++ WProgressHUD/WProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  WProgressHUD.h
+//  WProgressHUD
+//
+//  Created by winter on 2020/2/12.
+//  Copyright © 2020 winter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WProgressHUD.
+FOUNDATION_EXPORT double WProgressHUDVersionNumber;
+
+//! Project version string for WProgressHUD.
+FOUNDATION_EXPORT const unsigned char WProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WProgressHUD/PublicHeader.h>
+
+
