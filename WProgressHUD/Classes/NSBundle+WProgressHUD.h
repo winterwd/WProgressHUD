66ea3434a731b41e67f6763f90e0fd8bed8b2e0f
wakaka <wakaka@wakaka.com>
2023-12-15 14:31:01 +0800

    add localizable

---
+++ WProgressHUD/Classes/NSBundle+WProgressHUD.h
@@ -0,0 +1,17 @@
+//
+//  NSBundle+WProgressHUD.h
+//  WProgressHUD
+//
+//  Created by winter on 2023/12/15.
+//  Copyright © 2023 winter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSBundle (WProgressHUD)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WProgressHUD/Classes/NSBundle+WProgressHUD.m
@@ -0,0 +1,13 @@
+//
+//  NSBundle+WProgressHUD.m
+//  WProgressHUD
+//
+//  Created by winter on 2023/12/15.
+//  Copyright © 2023 winter. All rights reserved.
+//
+
+#import "NSBundle+WProgressHUD.h"
+
+@implementation NSBundle (WProgressHUD)
+
+@end
